1040c86a29b60b9643c4715ffdf5927689d259a1
wjdals95 <wjdals992@naver.com>
2023-01-31 15:49:00 +0900

    전체 양식 유효성 관리하기

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -10,6 +10,11 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
 
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
 
@@ -81,7 +86,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
